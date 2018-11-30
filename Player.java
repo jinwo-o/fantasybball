ae4a80efa86b74c22a5169f6f3afeb85841e196d
Hwang <jw4hwang@uwaterloo.ca>
2018-11-30 14:46:41 -0500

    Test Contributions

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player {
+	private int gamesToPlay;
+	private double points;
+	private double rebounds;
+	private double assists;
+	private double 3PM;
+	private double steals;
+	private double blocks;
+	private double turnovers;
+	private double dd;
+}
