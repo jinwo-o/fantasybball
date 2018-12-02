8ef6df22924e97b0797f49157f19e76025ff462a
Hwang <JIN_HWANG@homedepot.com>
2018-12-02 14:40:20 -0500

    Stat Constants

---
+++ PlayerWeeklyStats/src/StatsConstants.java
@@ -0,0 +1,13 @@
+public class StatsConstants {
+
+    public static final String PLAYER_NAME = "playerName";
+    public static final String POINTS = "points";
+    public static final String THREES = "threes";
+    public static final String REBOUNDS = "rebounds";
+    public static final String ASSISTS = "assists";
+    public static final String STEALS = "steals";
+    public static final String BLOCKS = "blocks";
+    public static final String TURNOVERS = "turnovers";
+    public static final String DD = "doubleDoubles";
+
+}
