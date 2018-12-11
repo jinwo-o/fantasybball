b17e091e5e90b66394e930e4d58d8e45a15abe9c
Hwang <JIN_HWANG@homedepot.com>
2018-12-11 16:26:47 -0500

    Added main file

---
+++ TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/TeamWeeklyStats.java
@@ -0,0 +1,9 @@
+package ca.fantasybasketball.teamweeklystats;
+
+public class TeamWeeklyStats {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/readers/ReadCSV.java
+++ TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/readers/ReadCSV.java
@@ -1,45 +1,43 @@
 package ca.fantasybasketball.teamweeklystats.readers;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+//import com.opencsv.CSVReader;
+
 
 public class ReadCSV {
 
     private ReadCSV() {}
 
     public static void CSVReader() {
-        String csvFile = "/Users/JXH3JJU/fantasy/FantasyCSV.csv";
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
-
-        try {
-
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-
-                // use comma as separator
-                String[] player = line.split(cvsSplitBy);
-
-                System.out.println("Player [name= " + player[0] + " , PPG=" + player[1] + "]");
-
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+//        String csvFile = "/Users/JXH3JJU/fantasy/FantasyCSV.csv";
+//        BufferedReader br = null;
+//        String line = "";
+//        String cvsSplitBy = ",";
+//
+//        try {
+//
+//            br = new BufferedReader(new FileReader(csvFile));
+//            while ((line = br.readLine()) != null) {
+//
+//                // use comma as separator
+//                String[] player = line.split(cvsSplitBy);
+//
+//                System.out.println("Player [name= " + player[0] + " , PPG=" + player[1] + "]");
+//
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (br != null) {
+//                try {
+//                    br.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
     }
 
 }
