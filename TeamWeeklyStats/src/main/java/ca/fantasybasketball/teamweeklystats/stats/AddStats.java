58d93a5cd592ba0a5b09837f888565e99a77acf2
Hwang <JIN_HWANG@homedepot.com>
2018-12-12 14:08:31 -0500

    Created a class to add stats classes

---
+++ TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/stats/AddStats.java
@@ -0,0 +1,22 @@
+package ca.fantasybasketball.teamweeklystats.stats;
+
+public class AddStats {
+
+    private AddStats()
+    {
+    }
+
+    // add total games played
+    public static Stats total(Stats total, Stats current){
+        total.setPoints(total.getPoints() + current.getPoints());
+        total.setThrees(total.getThrees() + current.getThrees());
+        total.setRebounds(total.getRebounds() + current.getRebounds());
+        total.setAssists(total.getAssists() + current.getAssists());
+        total.setSteals(total.getSteals() + current.getSteals());
+        total.setBlocks(total.getBlocks() + current.getBlocks());
+        total.setTurnovers(total.getTurnovers() + current.getTurnovers());
+        total.setDD(total.getDD() + current.getDD());
+
+        return total;
+    }
+}
