cb5e36ac6ccb3556411ce78212e90aeaae73732d
unknown <jw4hwang@uwaterloo.ca>
2019-01-23 09:01:09 -0500

    Updated issues

--- TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/TeamWeeklyStats.java
+++ TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/TeamWeeklyStats.java
@@ -1,34 +1,34 @@
 package ca.fantasybasketball.teamweeklystats;
 
-import ca.fantasybasketball.teamweeklystats.model.Player;
-import ca.fantasybasketball.teamweeklystats.stats.PlayerWeeklyTotal;
-import ca.fantasybasketball.teamweeklystats.readers.ReadCSV;
-import ca.fantasybasketball.teamweeklystats.model.Roster;
-import ca.fantasybasketball.teamweeklystats.stats.AddStats;
-import ca.fantasybasketball.teamweeklystats.model.Stats;
-
-import java.io.IOException;
-
-//public class TeamWeeklyStats {
+//import ca.fantasybasketball.teamweeklystats.model.Player;
+//import ca.fantasybasketball.teamweeklystats.stats.PlayerWeeklyTotal;
+//import ca.fantasybasketball.teamweeklystats.readers.ReadCSV;
+//import ca.fantasybasketball.teamweeklystats.model.Roster;
+//import ca.fantasybasketball.teamweeklystats.stats.AddStats;
+//import ca.fantasybasketball.teamweeklystats.model.Stats;
 //
-//    public static void main(String[] args) throws IOException {
+//import java.io.IOException;
 //
-//        Roster roster = ReadCSV.CSV_Reader("/Users/JXH3JJU/fantasy/FantasyCSVWeek10_SQUANCH.csv");
-//        Stats totalStats = new Stats();
-//        for(Player player : roster.getPlayers()) {
-//            Stats stats = PlayerWeeklyTotal.calculateWeeklyTotal(player);
-//            totalStats = AddStats.total(totalStats, stats);
-//        }
-//
-//        System.out.println("Minutes: "+ totalStats.getMinutes());
-//        System.out.println("Threes: " + totalStats.getThrees());
-//        System.out.println("Rebounds: " + totalStats.getRebounds());
-//        System.out.println("Assists: " + totalStats.getAssists());
-//        System.out.println("Steals: " + totalStats.getSteals());
-//        System.out.println("Blocks: " + totalStats.getBlocks());
-//        System.out.println("Turnovers: " + totalStats.getTurnovers());
-//        System.out.println("DD: " + totalStats.getDD());
-//        System.out.println("Points: " + totalStats.getPoints());
-//    }
-//
-//}
+////public class TeamWeeklyStats {
+////
+////    public static void main(String[] args) throws IOException {
+////
+////        Roster roster = ReadCSV.CSV_Reader("/Users/JXH3JJU/fantasy/FantasyCSVWeek10_SQUANCH.csv");
+////        Stats totalStats = new Stats();
+////        for(Player player : roster.getPlayers()) {
+////            Stats stats = PlayerWeeklyTotal.calculateWeeklyTotal(player);
+////            totalStats = AddStats.total(totalStats, stats);
+////        }
+////
+////        System.out.println("Minutes: "+ totalStats.getMinutes());
+////        System.out.println("Threes: " + totalStats.getThrees());
+////        System.out.println("Rebounds: " + totalStats.getRebounds());
+////        System.out.println("Assists: " + totalStats.getAssists());
+////        System.out.println("Steals: " + totalStats.getSteals());
+////        System.out.println("Blocks: " + totalStats.getBlocks());
+////        System.out.println("Turnovers: " + totalStats.getTurnovers());
+////        System.out.println("DD: " + totalStats.getDD());
+////        System.out.println("Points: " + totalStats.getPoints());
+////    }
+////
+////}
