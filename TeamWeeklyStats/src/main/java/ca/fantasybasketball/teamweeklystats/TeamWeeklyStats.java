83fa27127e670acbc7a1961b6911bb0a38385dae
unknown <jw4hwang@uwaterloo.ca>
2019-01-24 17:45:08 -0500

    Updated issues

--- TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/TeamWeeklyStats.java
+++ TeamWeeklyStats/src/main/java/ca/fantasybasketball/teamweeklystats/TeamWeeklyStats.java
@@ -9,26 +9,26 @@ package ca.fantasybasketball.teamweeklystats;
 //
 //import java.io.IOException;
 //
-////public class TeamWeeklyStats {
-////
-////    public static void main(String[] args) throws IOException {
-////
-////        Roster roster = ReadCSV.CSV_Reader("/Users/JXH3JJU/fantasy/FantasyCSVWeek10_SQUANCH.csv");
-////        Stats totalStats = new Stats();
-////        for(Player player : roster.getPlayers()) {
-////            Stats stats = PlayerWeeklyTotal.calculateWeeklyTotal(player);
-////            totalStats = AddStats.total(totalStats, stats);
-////        }
-////
-////        System.out.println("Minutes: "+ totalStats.getMinutes());
-////        System.out.println("Threes: " + totalStats.getThrees());
-////        System.out.println("Rebounds: " + totalStats.getRebounds());
-////        System.out.println("Assists: " + totalStats.getAssists());
-////        System.out.println("Steals: " + totalStats.getSteals());
-////        System.out.println("Blocks: " + totalStats.getBlocks());
-////        System.out.println("Turnovers: " + totalStats.getTurnovers());
-////        System.out.println("DD: " + totalStats.getDD());
-////        System.out.println("Points: " + totalStats.getPoints());
-////    }
-////
-////}
+public class TeamWeeklyStats {
+
+    public static void main(String[] args) throws IOException {
+
+        Roster roster = ReadCSV.CSV_Reader("/Users/JXH3JJU/fantasy/FantasyCSVWeek10_SQUANCH.csv");
+        Stats totalStats = new Stats();
+        for(Player player : roster.getPlayers()) {
+            Stats stats = PlayerWeeklyTotal.calculateWeeklyTotal(player);
+            totalStats = AddStats.total(totalStats, stats);
+        }
+
+        System.out.println("Minutes: "+ totalStats.getMinutes());
+        System.out.println("Threes: " + totalStats.getThrees());
+        System.out.println("Rebounds: " + totalStats.getRebounds());
+        System.out.println("Assists: " + totalStats.getAssists());
+        System.out.println("Steals: " + totalStats.getSteals());
+        System.out.println("Blocks: " + totalStats.getBlocks());
+        System.out.println("Turnovers: " + totalStats.getTurnovers());
+        System.out.println("DD: " + totalStats.getDD());
+        System.out.println("Points: " + totalStats.getPoints());
+    }
+
+}
